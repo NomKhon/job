de2c237befcb64b0698c80559c1e556b7a416fbb
TANAPON THONGNGAM <mimmixkung55az@gmail.com>
2026-06-29 16:44:21 +0700

    Add files via upload

---
+++ Hell yeahh/script.js
@@ -0,0 +1,11 @@
+for(i=1;i<5;i++){
+    document.write("<h",i,">","Computer Science @NPRU <br>","</h",1,">");
+}
+
+
+for( i=1;j<=10;i++ ) {
+    for(j=1;j<=1;j++) {
+        document.write("<Img src ='cat.png'width=60>");
+    }
+    document.write("<br>");
+}
